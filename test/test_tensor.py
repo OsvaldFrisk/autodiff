89349a14811a736a8bf19b13f72b1834a536c54a
Osvald Frisk <osvald.frisk@gmail.com>
2021-01-08 00:16:09 +0100

    added testing helper function for generating random tensors of specified shape

--- test/test_tensor.py
+++ test/test_tensor.py
@@ -1,8 +1,16 @@
 import unittest
 import numpy as np
 
+from typing import Tuple
+
 from numpy.testing import assert_array_almost_equal
 from autodiff.tensor import Tensor, Tensorable
+
+
+def rtensor(*shape: Tuple, requires_gradient: bool = True) -> Tensor:
+    """Instansites a tensor with randomly sampled values from uniform
+    distribution between [0,1] with specified shape"""
+    return Tensor(np.random.rand(*shape), requires_gradient=requires_gradient)
 
 
 def tensor_assert(
@@ -12,7 +20,8 @@ def tensor_assert(
     err_msg: str = '',
     verbose: bool = True
 ) -> None:
-    """Raises assert error if two tensors are not equal up to specified decimal precision"""
+    """Raises assert error if two tensors are not equal up to specified
+    decimal precision"""
     if type(t1) == Tensor:
         t1 = t1.data
     if type(t2) == Tensor:
@@ -33,7 +42,7 @@ class TestTensor(unittest.TestCase):
     def test_tensor_backward_with_gradient(self):
         t = Tensor([1, 2, 3], requires_gradient=True)
         t.backward(Tensor(2.))
-        tensor_assert(t.gradient.data, [2, 2, 2])
+        tensor_assert(t.gradient, [2, 2, 2])
 
     def test_tensoroperator_sum(self):
         t = Tensor([1, 2, 3], requires_gradient=True)
@@ -46,17 +55,17 @@ class TestTensor(unittest.TestCase):
 
         (t1 + t2).backward(Tensor([1., 2., 3.]))
 
-        tensor_assert(t1.gradient.data, [1, 2, 3])
-        tensor_assert(t2.gradient.data, [1, 2, 3])
+        tensor_assert(t1.gradient, [1, 2, 3])
+        tensor_assert(t2.gradient, [1, 2, 3])
 
     def test_tensor_add_broadcasted(self):
         t1 = Tensor([[1, 2, 3],
                      [4, 5, 6]],
-                     requires_gradient=True)
+                    requires_gradient=True)
         t2 = Tensor([1, 1, 1], requires_gradient=True)
 
         (t1 + t2).backward(Tensor([1, 1, 1]))
 
-        tensor_assert(t1.gradient.data, [[1, 1, 1],
-                                         [1, 1, 1]])
-        tensor_assert(t2.gradient.data, [1, 1, 1])
+        tensor_assert(t1.gradient, [[1, 1, 1],
+                                    [1, 1, 1]])
+        tensor_assert(t2.gradient, [1, 1, 1])
