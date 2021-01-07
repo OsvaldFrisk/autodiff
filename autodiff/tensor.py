2b4c41066d4c9dd619019c7499ef91285d259af1
Osvald Frisk <osvald.frisk@gmail.com>
2021-01-07 23:32:11 +0100

    added and tested tensor.backward and first tensor operator, sum

--- autodiff/tensor.py
+++ autodiff/tensor.py
@@ -1,10 +1,12 @@
 from typing import Callable, NamedTuple, Optional, Union, List
 import numpy as np
 
+
 class Adjoint(NamedTuple):
     """partial derivatives of the output with respect to the intermediate variables"""
     tensor: 'Tensor'
     gradient_func: Callable[['Tensor'], 'Tensor']
+
 
 Tensorable = Union[np.ndarray, list, float]
 
@@ -14,6 +16,7 @@ def ensure_tensor(tensorable: Tensorable):
         return tensorable
     else:
         return np.array(tensorable)
+
 
 class Tensor:
     def __init__(self,
@@ -29,5 +32,52 @@ class Tensor:
         if self.requires_gradient:
             self.zero_gradient()
 
+    @property
+    def data(self) -> np.ndarray:
+        return self._data
+
+    @data.setter
+    def data(self, new_data: np.ndarray) -> None:
+        self._data = new_data
+        # setting data invalidates gradient
+        self.gradient = None
+
     def zero_gradient(self) -> None:
         self.gradient = Tensor(np.zeros(self.shape, dtype=float))
+
+    def backward(self, gradient: 'Tensor' = None) -> None:
+        assert self.requires_gradient, "backward was called on a tensor that does not require a gradient"
+
+        if gradient is None:
+            if self.shape == ():
+                gradient = Tensor(1.)
+            else:
+                raise RuntimeError("No gradient specified")
+        
+        self.gradient.data = self.gradient.data + gradient.data  # type: ignore
+
+        for dependency in self.depends_on:
+            backward_gradient = dependency.gradient_func(gradient.data)
+            dependency.tensor.backward(Tensor(backward_gradient))
+
+    def sum(self) -> 'Tensor':
+        return _sum(self)
+
+
+# Tensor operations
+def _sum(tensor: 'Tensor') -> 'Tensor':
+    """
+    Inputs a tensor to sum, returns 0-tensor sum of elements.
+    """
+    data = tensor.data.sum()
+    requires_gradient = tensor.requires_gradient
+    depends_on: List[Adjoint] = []
+
+    if requires_gradient:
+
+        def gradient_func_sum(gradient: 'Tensor') -> 'Tensor':
+            return gradient * np.ones(tensor.shape).data
+
+        depends_on.append(Adjoint(tensor, gradient_func_sum))
+
+    return Tensor(data, requires_gradient, depends_on)
--- test/test_tensor.py
+++ test/test_tensor.py
@@ -1,14 +1,36 @@
 import unittest
 import numpy as np
 
-from numpy.testing import assert_array_almost_equal as tensor_assert
-from autodiff.tensor import Tensor
+from numpy.testing import assert_array_almost_equal
+from autodiff.tensor import Tensor, Tensorable
+
+
+def tensor_assert(
+    t1: Tensorable,
+    t2: Tensorable,
+    decimal: int = 6,
+    err_msg: str = '',
+    verbose: bool = True
+) -> None:
+    """Raises assert error if two tensors are not equal up to specified decimal precision"""
+    if type(t1) == Tensor:
+        t1 = t1.data
+    if type(t2) == Tensor:
+        t2 = t2.data
+
+    assert_array_almost_equal(t1, t2)
+
 
 class TestTensor(unittest.TestCase):
     def test_empty_tensor(self):
         Tensor([])
-       
+
     def test_tensor_instantiation(self):
         Tensor(1., requires_gradient=True)
         Tensor([1, 2, 3], requires_gradient=True)
         Tensor(np.array([1, 2, 3]), requires_gradient=True)
+
+    def test_tensoroperator_sum(self):
+        t = Tensor([1, 2, 3], requires_gradient=True)
+        t.sum().backward()
+        tensor_assert(t.gradient.data, [1, 1, 1])
